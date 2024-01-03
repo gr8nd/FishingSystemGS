43a813ea14c98430f51d2b77f8b718d08db931c1
Okorie Ndubuisi <swtworm@gmail.com>
2024-01-03 22:30:14 +0100

    Initial commit

--- app/src/main/java/com/mustapha/fishingsystemgs/activities/ViewPGRActivity.java
+++ app/src/main/java/com/mustapha/fishingsystemgs/activities/ViewPGRActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 
 import com.mustapha.fishingsystemgs.R;
 import com.mustapha.fishingsystemgs.adapters.ViewPGRAdapter;
+import com.mustapha.fishingsystemgs.classes.PGR;
+import com.mustapha.fishingsystemgs.databases.PGRDatabase;
+
+import java.util.List;
 
 public class ViewPGRActivity extends AppCompatActivity {
 
@@ -15,8 +19,12 @@ public class ViewPGRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pgractivity);
+        PGRDatabase pgrDb = new PGRDatabase(ViewPGRActivity.this,
+                "pgrs.db", null, 1);
+        List<PGR> pgrs = pgrDb.getPGRs();
 
         ViewPGRAdapter adapter = new ViewPGRAdapter(this);
+        adapter.setPGR(pgrs);
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ViewPGRActivity.this);
